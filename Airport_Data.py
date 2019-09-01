bfd0c608da10ce42ef342e0383999e251177618f
Joe Kastner <36719144+JoeKastner@users.noreply.github.com>
2019-08-31 21:51:33 -0400

    Update Airport_Data.py
    Hid Username and Password

--- Airport_Data.py
+++ Airport_Data.py
@@ -9,7 +9,7 @@ data = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/maste
 data.columns = ['AIRPORT_ID','NAME','CITY','COUNTRY','IATA','ICAO','LATITUDE','LONGITUDE','ALTITUDE','TIMEZONE','DST','TZ_DB','TYPE','SOURCE']
 data.set_index('AIRPORT_ID')
 #%% Create connection to PostgreSQL database
-engine = create_engine('postgresql+psycopg2://joek318:wolphin85@mydbinstance.c39czh0r7sit.us-east-2.rds.amazonaws.com/joek318')
+engine = create_engine('postgresql+psycopg2://{USERNAME}:{PASSWORD}@mydbinstance.c39czh0r7sit.us-east-2.rds.amazonaws.com/{USERNAME}')
 
 #%%
 data.to_sql('AIRPORT_DATA',con=engine)
